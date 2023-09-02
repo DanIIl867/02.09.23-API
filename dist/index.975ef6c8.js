1b2ac7fe9855dcbb19e36d8cf129235475124322
Daniil <negativdaniil4@gmail.com>
2023-09-02 13:02:40 +0300

    02.09.23-API

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -503,13 +503,35 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8lqZg":[function(require,module,exports) {
-const url = "https://newsapi.org/v2/everything?q=bitcoin";
-const options = {
-    headers: {
-        "X-Api-Key": "f51d51ef29634144b84cf933be09d34e"
-    }
-};
-fetch(url, options).then((response)=>response.json()).then(console.log);
+// const url = 'https://newsapi.org/v2/everything?q=bitcoin'
+// const options = {
+//     headers:{
+//         'X-Api-Key': 'f51d51ef29634144b84cf933be09d34e'
+//     }
+// }
+// fetch(url, options)
+// .then (response=>response.json())
+// .then(console.log)
+const form = document.querySelector(".js-search-form");
+function getImg(query) {
+    const url = `https://newsapi.org/v2/everything?q=${query}`;
+    const options = {
+        headers: {
+            "X-Api-Key": "bc9655a1ace34da58299b1782e8ed736"
+        }
+    };
+    return fetch(url, options).then((response)=>response.json());
+}
+form.addEventListener("submit", onSearchQuery);
+function onSearchQuery(e) {
+    e.preventDefault();
+    const query = e.currentTarget.elements.query.value;
+    getImg(query).then((articles)=>{
+        return articles.articles;
+    }).then((articles)=>{
+        console.log(articles[0].source.name);
+    });
+}
 
 },{}]},["1RB6v","8lqZg"], "8lqZg", "parcelRequire832d")
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-const url = 'https://newsapi.org/v2/everything?q=bitcoin'
-const options = {
-    headers:{
-        'X-Api-Key': 'f51d51ef29634144b84cf933be09d34e'
+// const url = 'https://newsapi.org/v2/everything?q=bitcoin'
+// const options = {
+//     headers:{
+//         'X-Api-Key': 'f51d51ef29634144b84cf933be09d34e'
+//     }
+// }
+
+// fetch(url, options)
+// .then (response=>response.json())
+// .then(console.log)
+
+
+
+const form = document.querySelector('.js-search-form')
+
+
+
+function getImg(query) {
+    const url = `https://newsapi.org/v2/everything?q=${query}`
+    const options = {
+        headers: {
+            'X-Api-Key': 'bc9655a1ace34da58299b1782e8ed736'
+        }
     }
+    return fetch(url,options)
+    .then(response => response.json())
 }
 
-fetch(url, options)
-.then (response=>response.json())
-.then(console.log)
+form.addEventListener('submit', onSearchQuery)
+
+function onSearchQuery(e) {
+    e.preventDefault()
+    const query = e.currentTarget.elements.query.value
+    getImg(query).then(articles => {
+        return articles.articles
+    })
+    .then(articles => {
+        console.log(articles[0].source.name);
+    })
+}
